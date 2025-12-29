2f18978d4794de49c347ade55f4fa0cdaaa988a5
Lim Joe Quan <joequanlim@gmail.com>
2025-12-29 22:24:18 +0800

    Store all code within master IIFE
    All global code has been removed except for master IIFE

--- javascript.js
+++ javascript.js
@@ -1,44 +1,48 @@
-const gameBoard = (function() {
-    let gameArray = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ]
-    function placeMarker(player) {
-        let row = --player.row
-        let col = --player.col
-        gameArray[row][col] = player.marker
-        console.log(gameArray)
-    }
-    return {gameArray, placeMarker}
+const game =  (function() {
+    const gameBoard = (function() {
+        let gameArray = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ]
+        function placeMarker(player) {
+            let row = --player.row
+            let col = --player.col
+            gameArray[row][col] = player.marker
+            console.log(gameArray)
+        }
+        return {gameArray, placeMarker}
+    })();
+
+
+    const gameManager = (function() {
+        //true: player1's turn ; false: player2's turn
+        let isPlayer1Turn;
+
+        function createPlayer(number, marker) {
+            let row;
+            let col;
+            function selectPlacement(selectedRow, selectedCol) {
+                this.row = Number(selectedRow)
+                this.col = Number(selectedCol)
+            }
+            return {number, marker, row, col, selectPlacement}
+        }
+
+        let player1 = createPlayer(1, "O")
+        let player2 = createPlayer(2, "X")
+        function checkGameState() {
+            let result = determineWinner()
+        }
+        return {isPlayer1Turn, player1, player2, checkGameState}
+    })();
+    return {gameManager, gameBoard}
 })();
-
-
-function createPlayer(number, marker) {
-    let row;
-    let col;
-    function selectPlacement(selectedRow, selectedCol) {
-        this.row = Number(selectedRow)
-        this.col = Number(selectedCol)
-    }
-    return {number, marker, row, col, selectPlacement}
-}
-
-
-//GLOBAL CODE BELOW
-let player1 = createPlayer(1, "O")
-let player2 = createPlayer(2, "X")
-
-
-
-
-
-
-
 
 
 
 
 //iife for gameManager
-//gameManager toggles boolean of players to determine turns
+//gameManager toggles a boolean to determine turns
+//gamemanager creates players
 //gamemanager checks for wins (arrays of coords) and draws (no win but all spaces are filled)
