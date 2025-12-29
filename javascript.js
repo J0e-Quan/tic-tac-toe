b9437c65e82daefc77c6d32c2ea2161006180cce
Lim Joe Quan <joequanlim@gmail.com>
2025-12-29 20:25:58 +0800

    Write psuedocode for game logic

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const gameBoard = (function() {
+    //create array for game grid
+    //method for accepting coord and symbol of player then placing it
+})
+
+//factory func for making players
+//players can input coords of the box for placing symbol
+
+//use factory fumc to make player1 and player2
+//each player has own symbol (capital X or capital O)
+
+//iife for gameManager
+//gameManager toggles boolean of players to determine turns
+//gamemanager checks for wins (arrays of coords) and draws (no win but all spaces are filled)
